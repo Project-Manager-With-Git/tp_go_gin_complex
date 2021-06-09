76418f4d1eb80077ffeb93cf83755f5d32ab746e
HUANG SIZHE <hsz1273327@gmail.com>
2021-06-09 10:52:21 +0800

    update

--- downloads/tablenamespace/tablenamespace.go
+++
@@ -1,9 +0,0 @@
-package tablenamespace
-
-import "github.com/gin-gonic/gin"
-
-func Init(group *gin.RouterGroup) {
-
-	uls := TableListSource{}
-	group.GET("", uls.Get)
-}
--- events/setting/setting.go
+++
@@ -1,9 +0,0 @@
-package setting
-
-import "time"
-
-var RedisQueryTimeout = time.Duration(50) * time.Millisecond
-
-func Init(redis_query_timeout_ms int) {
-	RedisQueryTimeout = time.Duration(redis_query_timeout_ms) * time.Millisecond
-}
